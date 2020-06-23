0cd5fe9e4af8920471b8ee3750ea2dfe15e7f1a8
Rob <rob@itsallinthecode.com>
2020-06-23 15:16:40 +0100

    Finish sign in and sign up handle

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 
-const Header = (currentUser) => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -17,9 +17,11 @@ const Header = (currentUser) => (
         CONTACT
       </Link>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
       ) : (
-        <Link className="option" to="/signin"></Link>
+        <Link className="option" to="/signin">SIGN IN</Link>
       )}
     </div>
   </div>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
   appId: "1:562528485240:web:73445bae0b475e7c4d128d",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+  console.log(snapshot);
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
